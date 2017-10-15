1be1c3c4352e5995a34ea30fe429ac58d21c21a3
izen448 <izen448@users.noreply.github.com>
2017-10-15 07:49:31 -0400

    Updated to support SSL.

--- jobs/bitcoin.rb
+++ jobs/bitcoin.rb
@@ -5,6 +5,7 @@ require 'uri'
 SCHEDULER.every '5s' do
   uri = URI.parse('https://api.coinbase.com/v2/prices/BTC-USD/spot')
   http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
   json_response = JSON.parse(response.body)
