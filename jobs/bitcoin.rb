5c472aeef4e8f76886880d1132138f1a9135e75c
jesse-alvey <11009635+jesse-alvey@users.noreply.github.com>
2017-11-19 10:44:06 -0600

    Update to bitcoin.rb
    Uses Coindesk price API instead of Coinbase. Updates less frequently but updates when initially loaded.

--- jobs/bitcoin.rb
+++ jobs/bitcoin.rb
@@ -2,14 +2,14 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-SCHEDULER.every '5s' do
-  uri = URI.parse('https://api.coinbase.com/v2/prices/BTC-USD/spot')
+SCHEDULER.every '120s', :first_in => 0 do |job|
+  uri = URI.parse('http://api.coindesk.com/v1/bpi/currentprice.json')
   http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
+  #http.use_ssl = true
   request = Net::HTTP::Get.new(uri.request_uri)
   response = http.request(request)
   json_response = JSON.parse(response.body)
-  btc_price = json_response['data']['amount']
+  btc_price = json_response['bpi']['USD']['rate']
   btc_price = '%.2f' % btc_price.delete(',').to_f
   #puts btc_price
   send_event('btcprice', { value: btc_price.to_f} )
